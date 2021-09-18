1fef9c5e1fe082f77a4c1f76a53dae29f90c82c3
minhhl <minhhl@japanquality.asia>
2021-09-18 21:56:13 +0700

    master_for_apdung

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'Huy Nguyễn';
   public age = 25;
+  public traiCay = ['Táo', 'Nho', 'Cam', 'Quít', 'Ổi'];
+  public traiCay2 = [
+    { ten: 'Táo', gia: 12, haGia: true },
+    { ten: 'Nho', gia: -3.234, haGia: false },
+    { ten: 'Cam', gia: 34.26, haGia: false },
+    { ten: 'Quít', gia: 32.45, haGia: true },
+  ];
+  constructor() { }
 
-  constructor() {}
+  ngOnInit(): void {
 
-  ngOnInit(): void {}
+    console.log('trai cay = ', this.traiCay2);
+  }
 
   public resetName(): void {
     console.log('resetName');
